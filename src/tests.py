cdc73ff601f79e224dc1b2ab0d50c67dc4c87b5c
Jørgen Høst <jorgenhost96@gmail.com>
2025-05-07 14:53:14 +0200

    comments on thesis

---
+++ src/tests.py
@@ -0,0 +1,24 @@
+import unittest
+from datetime import datetime
+
+from timeset import TimeRange
+
+start = datetime(2021, 5, 20, 12, 12)
+end = datetime(2021, 5, 20, 14, 12)
+string_repr = "TimeRange(start=datetime.datetime(2021, 5, 20, 12, 12), end=datetime.datetime(2021, 5, 20, 14, 12))"
+
+
+class TimeRangeInitializationTest(unittest.TestCase):
+
+    def test_initialization_with_start_and_end(self):
+        t = TimeRange(start=start, end=end)
+        self.assertEqual(str(t), string_repr)
+
+    def test_empty_initialization(self):
+        t = TimeRange()
+        self.assertEqual(str(t), "TimeRange()")    def test_start_end_integrity(self):
+        with self.assertRaises(ValueError):
+            TimeRange(start=end, end=start)
+
+if __name__ == '__main__':
+    unittest.main()
